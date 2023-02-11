82f663db8830cc54b9cf90916b217d5a2e3829f1
Marek Skolik <marek250296@interia.pl>
2023-02-11 12:16:50 +0100

    fourth task

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,68 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
-const { v4: uuidv4 } = require('uuid');
+const testimonialsController = require('../controllers/testimonials.controller');
 
-const database = db.testimonials;
-
-// get random testimonial
-router.route('/testimonials/random').get((req, res) => {
-  const random = Math.floor(Math.random() * database.length);
-  res.json(database[random]);
-});
-
-// get all testimonials
-router.route('/testimonials').get((req, res) => {
-  res.json(database);
-});
-
-// get testimonial by id
-router.route('/testimonials/:id').get((req, res) => {
-  const index = database.findIndex((element) => element.id == req.params.id);
-
-  if (index != -1) {
-    res.json(database[index]);
-  } else {
-    res.status(404).json({ message: 'Not found...' });
-  }
-});
-
-// add testimonial
-router.route('/testimonials').post((req, res) => {
-  database.push({
-    id: uuidv4(),
-    author: req.body.author,
-    text: req.body.text,
-  });
-
-  res.json({ message: 'OK' });
-});
-
-// modify testimonial by id
-router.route('/testimonials/:id').put((req, res) => {
-  const index = database.findIndex((element) => element.id == req.params.id);
-
-  if (index != -1) {
-    database[index] = {
-      ...database[index],
-      ...req.body,
-    };
-    res.json({ message: 'OK' });
-  } else {
-    res.status(404).json({ message: 'Not found...' });
-  }
-});
-
-// delete testimonial by id
-router.route('/testimonials/:id').delete((req, res) => {
-  const index = database.findIndex((element) => element.id == req.params.id);
-
-  if (index != -1) {
-    database.splice(index, 1);
-    res.json({ message: 'OK' });
-  } else {
-    res.status(404).json({ message: 'Not found...' });
-  }
-});
+router.get('/testimonials/random', testimonialsController.getRandom);
+router.get('/testimonials', testimonialsController.getAll);
+router.get('/testimonials/:id', testimonialsController.getById);
+router.post('/testimonials', testimonialsController.add);
+router.put('/testimonials/:id', testimonialsController.edit);
+router.delete('/testimonials/:id', testimonialsController.remove);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-// const { v4: uuidv4 } = require('uuid');
-// const db = require('./db');
 const cors = require('cors');
 const path = require('path');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
@@ -32,6 +31,16 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', {
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', (err) => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
